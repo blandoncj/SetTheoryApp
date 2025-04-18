cf4ed6b814b491f7c608d41f8dfdf53a8d2ebe1d
Jacobo Blandón Castro <jacoboblandon94@gmail.com>
2025-04-18 00:52:37 -0500

    feat(header): add Header component with basic styles
    Introduce a new Header component with a gradient background,
    centered title, and responsive design. This component serves as
    the main header for the application, providing a visually appealing
    entry point.
    
    - Added `Header.tsx` with a styled header element.
    - Created an index file for easier imports.
    
    This change lays the foundation for a consistent UI structure.

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="flex items-center justify-center p-6 mx-20 rounded-lg border-2 border-cyan-300 bg-gradient-to-r from-gray-800 via-gray-900 to-black shadow-lg">
+      <h1 className="font-semibold text-4xl text-white tracking-wide">
+        Set Theory App
+      </h1>
+    </header>
+  )
+}
